7099ded19afc954bbf092687d54a13bdb42b6224
mahdi-jafaree <mehdi.jafaree1375@gmail.com>
2021-01-07 12:12:04 +0430

    shit

--- src/main/webapp/app/entities/index.tsx
+++ src/main/webapp/app/entities/index.tsx
@@ -4,7 +4,7 @@ import { Switch } from 'react-router-dom';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 
-/* jhipster-needle-add-route-import - JHipster will add routes here */
+/* jhipster-needle-add-route-import - JHipster will add routtttttes here */
 
 const Routes = ({ match }) => (
   <div>
